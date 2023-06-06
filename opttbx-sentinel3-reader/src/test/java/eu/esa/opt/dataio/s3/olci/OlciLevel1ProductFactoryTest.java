44ec86a2ea2f424a54e14ffa850cd4cf8f07b24c
Marco Peters <marco.peters@brockmann-consult.de>
2023-06-06 15:59:52 +0200

    [SNAP-3519] Grouping for OLCI bands need to consider renaming of error bands

---
+++ opttbx-sentinel3-reader/src/test/java/eu/esa/opt/dataio/s3/olci/OlciLevel1ProductFactoryTest.java
@@ -0,0 +1,33 @@
+package eu.esa.opt.dataio.s3.olci;
+
+import com.bc.ceres.annotation.STTM;
+import org.esa.snap.core.datamodel.Product;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class OlciLevel1ProductFactoryTest {
+
+    @STTM("SNAP-3519")
+    @Test
+    public void testBandGroupingPattern() {
+        String bandGroupingPattern = OlciLevel1ProductFactory.BAND_GROUPING_PATTERN;
+        Product dummy = new Product("name", "type", 10, 10);
+        dummy.setAutoGrouping(bandGroupingPattern);
+        Product.AutoGrouping autoGrouping = dummy.getAutoGrouping();
+
+        assertNotEquals(-1, autoGrouping.indexOf("Oa02_radiance"));
+        assertNotEquals(-1, autoGrouping.indexOf("Oa20_radiance"));
+        assertNotEquals(-1, autoGrouping.indexOf("Oa13_radiance_err"));
+        assertNotEquals(-1, autoGrouping.indexOf("Oa13_radiance_unc"));
+        assertNotEquals(-1, autoGrouping.indexOf("Oa01_radiance_unc"));
+        assertNotEquals(-1, autoGrouping.indexOf("atmospheric_temperature_profile_pressure_level_3"));
+        assertNotEquals(-1, autoGrouping.indexOf("lambda0_band_7"));
+        assertNotEquals(-1, autoGrouping.indexOf("FWHM_band_3"));
+        assertNotEquals(-1, autoGrouping.indexOf("solar_flux_band_19"));
+        assertEquals(-1, autoGrouping.indexOf("not_handled"));
+
+
+    }
+}
