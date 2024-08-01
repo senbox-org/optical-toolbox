84118dc57941463d7bfd6193860705cfb46564ed
Tom <tom.block@brockmann-consult.de>
2024-08-01 09:38:30 +0200

    corrected issue with "Derived from ..."

---
+++ opttbx-rad2refl/src/test/java/eu/esa/opt/processor/rad2refl/Rad2ReflAuxdataTest.java
@@ -0,0 +1,19 @@
+package eu.esa.opt.processor.rad2refl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Rad2ReflAuxdataTest {
+
+    @Test
+    public void testGetResolutionType() {
+        assertEquals(ResolutionType.REDUCED, Rad2ReflAuxdata.getResolutionType("MER_RR__1P"));
+        assertEquals(ResolutionType.REDUCED, Rad2ReflAuxdata.getResolutionType("Derived from(MER_RR__1P)"));
+
+        assertEquals(ResolutionType.FULL, Rad2ReflAuxdata.getResolutionType("MER_FR__1P"));
+        assertEquals(ResolutionType.FULL, Rad2ReflAuxdata.getResolutionType("Derived from(MER_FRS_1P)"));
+
+        assertEquals(ResolutionType.UNKNOWN, Rad2ReflAuxdata.getResolutionType("STRANGE-TYPE"));
+    }
+}
