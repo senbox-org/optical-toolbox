e6df0c0c8eb7a8d5df61c53000e2a2ca0ea6f12c
Tom <tom.block@brockmann-consult.de>
2024-11-14 10:19:57 +0100

    resolves SNAP-3882 - merged from 11.x

--- opttbx-seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/L2ProductReaderPlugIn.java
+++ opttbx-seadas-reader/src/main/java/gov/nasa/gsfc/seadas/dataio/L2ProductReaderPlugIn.java
@@ -88,25 +88,14 @@ public class L2ProductReaderPlugIn implements ProductReaderPlugIn {
      */
     @Override
     public DecodeQualification getDecodeQualification(Object input) {
-        final File file = SeadasProductReader.getInputFile(input);
-        if (file == null) {
-            return DecodeQualification.UNABLE;
+        final File inputFile = SeadasHelper.getInputFile(input);
+
+        final DecodeQualification decodeQualification = SeadasHelper.checkInputObject(inputFile);
+        if (decodeQualification == DecodeQualification.UNABLE) {
+            return decodeQualification;
         }
-        if (!file.exists()) {
-            if (DEBUG) {
-                System.out.println("# File not found: " + file);
-            }
-            return DecodeQualification.UNABLE;
-        }
-        if (!file.isFile()) {
-            if (DEBUG) {
-                System.out.println("# Not a file: " + file);
-            }
-            return DecodeQualification.UNABLE;
-        }
-        NetcdfFile ncfile = null;
-        try {
-            ncfile = NetcdfFileOpener.open(file.getPath());
+
+        try (NetcdfFile ncfile = NetcdfFileOpener.open(inputFile.getPath())) {
             if (ncfile != null) {
                 String titleattr = "title";
                 Attribute titleAttribute = ncfile.findGlobalAttributeIgnoreCase(titleattr);
@@ -115,11 +104,11 @@ public class L2ProductReaderPlugIn implements ProductReaderPlugIn {
                     Attribute instrumentAttribute = ncfile.findGlobalAttribute("instrument");
                     Attribute platformAttribute = ncfile.findGlobalAttribute("platform");
                     Attribute levelAttribute = ncfile.findGlobalAttribute("processing_level");
-                    if(instrumentAttribute != null && platformAttribute != null  && levelAttribute != null) {
+                    if (instrumentAttribute != null && platformAttribute != null && levelAttribute != null) {
                         final String level = levelAttribute.getStringValue();
-                        if(level.equals("L2")) {
+                        if (level.equals("L2")) {
                             if (DEBUG) {
-                                System.out.println(file);
+                                System.out.println(inputFile);
                             }
                             ncfile.close();
                             return DecodeQualification.INTENDED;
@@ -129,41 +118,34 @@ public class L2ProductReaderPlugIn implements ProductReaderPlugIn {
                         }
 
                     } else {
-                    final String title = titleAttribute.getStringValue();
-                    if (title != null) {
-                        if (supportedProductTypeSet.contains(title.trim())) {
-                            if (DEBUG) {
-                                System.out.println(file);
-                            }
-                            ncfile.close();
-                            return DecodeQualification.INTENDED;
-                        } else {
-                            if (DEBUG) {
-                                System.out.println("# Unrecognized attribute Title=[" + title + "]: " + file);
+                        final String title = titleAttribute.getStringValue();
+                        if (title != null) {
+                            if (supportedProductTypeSet.contains(title.trim())) {
+                                if (DEBUG) {
+                                    System.out.println(inputFile);
+                                }
+                                ncfile.close();
+                                return DecodeQualification.INTENDED;
+                            } else {
+                                if (DEBUG) {
+                                    System.out.println("# Unrecognized attribute Title=[" + title + "]: " + inputFile);
+                                }
                             }
                         }
                     }
-                    }
                 } else {
                     if (DEBUG) {
-                        System.out.println("# Missing attribute 'Title': " + file);
+                        System.out.println("# Missing attribute 'Title': " + inputFile);
                     }
                 }
             } else {
                 if (DEBUG) {
-                    System.out.println("# Can't open as NetCDF: " + file);
+                    System.out.println("# Can't open as NetCDF: " + inputFile);
                 }
             }
         } catch (Exception ignore) {
             if (DEBUG) {
-                System.out.println("# I/O exception caught: " + file);
-            }
-        } finally {
-            if (ncfile != null) {
-                try {
-                    ncfile.close();
-                } catch (IOException ignore) {
-                }
+                System.out.println("# I/O exception caught: " + inputFile);
             }
         }
         return DecodeQualification.UNABLE;
