20f7b1dcdf2fce20c94069eba8a86addd86bac84
belu-23 <benjamin.lutz936@gmail.com>
2025-07-01 10:15:16 +0200

    refactor geo coding

--- opttbx-sentinel3-reader/src/main/java/eu/esa/opt/dataio/s3/synergy/AODProductFactory.java
+++ opttbx-sentinel3-reader/src/main/java/eu/esa/opt/dataio/s3/synergy/AODProductFactory.java
@@ -5,14 +5,7 @@ import eu.esa.opt.dataio.s3.AbstractProductFactory;
 import eu.esa.opt.dataio.s3.manifest.Manifest;
 import eu.esa.opt.dataio.s3.Sentinel3ProductReader;
 import eu.esa.opt.dataio.s3.util.S3NetcdfReader;
-import eu.esa.opt.dataio.s3.util.S3Util;
-import org.esa.snap.core.dataio.geocoding.ComponentFactory;
-import org.esa.snap.core.dataio.geocoding.ComponentGeoCoding;
-import org.esa.snap.core.dataio.geocoding.ForwardCoding;
-import org.esa.snap.core.dataio.geocoding.GeoChecks;
-import org.esa.snap.core.dataio.geocoding.GeoRaster;
-import org.esa.snap.core.dataio.geocoding.InverseCoding;
-import org.esa.snap.core.dataio.geocoding.util.RasterUtils;
+import org.esa.snap.core.dataio.geocoding.*;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.Product;
 
@@ -49,39 +42,13 @@ public class AODProductFactory extends AbstractProductFactory {
 
     @Override
     protected void setGeoCoding(Product targetProduct) throws IOException {
-        final String lonVariableName = "longitude";
-        final String latVariableName = "latitude";
-        final Band lonBand = targetProduct.getBand(lonVariableName);
-        final Band latBand = targetProduct.getBand(latVariableName);
+        final Band lonBand = targetProduct.getBand("longitude");
+        final Band latBand = targetProduct.getBand("latitude");
         if (lonBand == null || latBand == null) {
             return;
         }
-
-        final double[] longitudes = RasterUtils.loadGeoData(lonBand);
-        final double[] latitudes = RasterUtils.loadGeoData(latBand);
-
-        // replace fill value with NaN
-        for (int i = 0; i < longitudes.length; i++) {
-            if (longitudes[i] <= FILL_VALUE) {
-                longitudes[i] = Double.NaN;
-            }
-            if (latitudes[i] <= FILL_VALUE) {
-                latitudes[i] = Double.NaN;
-            }
-        }
-
-        final int width = targetProduct.getSceneRasterWidth();
-        final int height = targetProduct.getSceneRasterHeight();
-        final GeoRaster geoRaster = new GeoRaster(longitudes, latitudes, lonVariableName, latVariableName,
-                                                  width, height, RESOLUTION_IN_KM);
-
-        final String[] keys = S3Util.getForwardAndInverseKeys_pixelCoding(SYSPROP_SYN_AOD_PIXEL_GEO_CODING_INVERSE);
-        final ForwardCoding forward = ComponentFactory.getForward(keys[0]);
-        final InverseCoding inverse = ComponentFactory.getInverse(keys[1]);
-
-        final ComponentGeoCoding geoCoding = new ComponentGeoCoding(geoRaster, forward, inverse, GeoChecks.POLES);
-        geoCoding.initialize();
-
+        ComponentGeoCoding geoCoding =
+                GeoCodingFactory.createPixelGeoCoding(latBand, lonBand, RESOLUTION_IN_KM);
         targetProduct.setSceneGeoCoding(geoCoding);
     }
 
